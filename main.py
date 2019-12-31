303c04eac7d5ea2c6bd3ba75abb81f52a8761e73
Arne Jacobs <arne.jacobs@ugent.be>
2019-12-31 17:54:08 +0100

    changed a small thing

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from word_generator import WordGenerator
 def main():
     word_generator = WordGenerator(False)
 
-    for i in range(3, 4):
+    for i in range(1, 4):
         markov_dict = MarkovDict(lookback_amount=i, lookahead_amount=1)
         markov_dict.process_file("lorem_ipsum.txt")
         markov_dict.save_to_file(f"./dicts/lorem_ipsum_{i}.markov")
 
-        word_generator.add_dictionary(markov_dict, weight=1)
+        word_generator.add_dictionary(markov_dict, weight=4-i)
 
     sentence = ""
     for i in range(5):
